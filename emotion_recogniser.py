e51508e61f8f7b939507f39f9abf4823dcb39464
Shashi Kant <shashikg.iitk@gmail.com>
2018-12-09 02:14:14 +0530

    bug fx (preprocessing of faces) and txt display

--- emotion_recogniser.py
+++ emotion_recogniser.py
@@ -11,24 +11,32 @@ import matplotlib.pyplot as plt
 img_rows, img_cols = 48, 48
 # 0=Angry, 1=Disgust, 2=Fear, 3=Happy, 4=Sad, 5=Surprise, 6=Neutral
 emotion = ['Angry', "Disgust", 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+font = cv2.FONT_HERSHEY_SIMPLEX
 
 model = []
+print('Loading Models...')
+print('0/3')
 for i in range(2):
     m = load_model('saved_model/' + 'cnn'+str(i)+'.h5')
-    print('cnn '+str(i))
+    print(str(i+1) + '/3')
     model.append(m)
+
 m = load_model('saved_model/ensemble.h5')
 model.append(m)
+print('3/3')
 
-print("Models Loaded...")
+print("Loading Complete!")
 
 def predict(x):
     x_rev = np.flip(x, 1)
+    x = x.astype('float32')
+    x_rev = x_rev.astype('float32')
+    x /= 255
+    x_rev /= 255
     p = np.zeros((1, 14))
     p[:,0:7] = model[0].predict(x.reshape(1,48,48,1))
     p[:,7:14] = model[1].predict(x_rev.reshape(1,48,48,1))
     pre = model[2].predict(p)
-
     return pre
 
 cap = cv2.VideoCapture(0)
@@ -40,12 +48,13 @@ while(True):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     faces = get_faces(gray)
-    for i, (x,y,face) in enumerate(faces):
-        cv2.imshow(str(i),face)
+    for i, (y,x,face) in enumerate(faces):
         pre = predict(face)
-        # print(pre)
-        print(emotion[np.argmax(pre)])
+        k = np.argmax(pre)
+        txt = emotion[k] + ' [' + str(int(pre[0,k]*100)) + '%]'
+        cv2.putText(img, txt, (x, y), font, 0.5,(0,255,0),1,cv2.LINE_AA)
 
+    cv2.imshow('Camera', img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
--- emotion_trainer.py
+++ emotion_trainer.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 import cv2
 
 # Load the datasets
-data = pd.read_csv('../input/fer20131.csv', delimiter=',')
+data = pd.read_csv('data/fer2013.csv', delimiter=',')
 
 # Taking Training and PublicTest data for training and PrivateTest data for testing
 data_train = data[:32298]
@@ -54,171 +54,171 @@ plt.imshow(x_train[0].reshape((48,48)))
 plt.figure(2)
 plt.imshow(x_train_rev[0].reshape((48,48)))
 
-# Some preprocessing
-input_shape = (img_rows, img_cols, 1)
-
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train_rev = x_train_rev.astype('float32')
-x_test_rev = x_test_rev.astype('float32')
-x_train /= 255
-x_test /= 255
-x_train_rev /= 255
-x_test_rev /= 255
-print('x_train shape:', x_train.shape)
-
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
-print('y_train shape:', y_train.shape)
-
-# define the model
-def cnn_model():
-    model = Sequential()
-
-    model.add(BatchNormalization(input_shape=input_shape))
-
-    model.add(Conv2D(32, kernel_size=(3, 3)))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-
-    model.add(Conv2D(64, (3, 3)))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-
-    model.add(Conv2D(512, (3, 3)))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-
-    model.add(Conv2D(64, (3, 3)))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(Dropout(0.25))
-    # model.add(Conv2D(32, (3, 3)))
-    # model.add(BatchNormalization())
-    # model.add(Activation('relu'))
-    # model.add(Dropout(0.2))
-
-    model.add(Flatten())
-    model.add(Dense(512))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(Dropout(0.25))
-
-    model.add(Dense(128))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(Dropout(0.25))
-
-    model.add(Dense(num_classes, activation='softmax'))
-
-    model.compile(loss=keras.losses.categorical_crossentropy,
-                  optimizer=keras.optimizers.adam(),
-                  metrics=['accuracy'])
-
-    return model
-
-# function to plot graph
-def plotGraph(history):
-    plt.plot(history.history['acc'])
-    plt.plot(history.history['val_acc'])
-    plt.title('model accuracy')
-    plt.ylabel('accuracy')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'])
-    plt.show()
-
-    # summarize history for loss
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title('model loss')
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'])
-    plt.show()
-
-# We will train two models one on normal images another on reversed images and finally a NN on predicted values from these models
-
-batch_size = 64
-epochs = 25
-model = []
-
-print("=======| Model 1 |=========")
-modelc = cnn_model()
-history = modelc.fit(x_train, y_train,
-          batch_size=batch_size,
-          epochs=epochs,
-          verbose=1,
-          validation_split = 0.1)
-model.append(modelc)
-plotGraph(history)
-
-print("=======| Model 2 |=========")
-modelc = cnn_model()
-history = modelc.fit(x_train_rev, y_train,
-          batch_size=batch_size,
-          epochs=epochs,
-          verbose=1,
-          validation_split = 0.1)
-model.append(modelc)
-plotGraph(history)
-
-# p_tr >> prediction on training data
-# p_te >> prediction on test data
-
-p_tr = []
-p_te = []
-
-for i, m in enumerate(model):
-    if i ==0:
-        p = m.predict(x_train)
-        pt = m.predict(x_test)
-    else:
-        p = m.predict(x_train_rev)
-        pt = m.predict(x_test_rev)
-    p_tr.append(p)
-    p_te.append(pt)
-#     m.save('cnn'+str(i)+'.h5')
-
-print(len(model))
-
-p_train = np.zeros((y_train.shape[0],num_classes*len(model)))
-p_test = np.zeros((y_test.shape[0],num_classes*len(model)))
-for i, p in enumerate(p_tr):
-    print(i)
-    p_train[:,num_classes*i:num_classes*(i+1)] = p
-
-for i, p in enumerate(p_te):
-    p_test[:,num_classes*i:num_classes*(i+1)] = p
-
-print(p_train.shape, p_test.shape)
-
-# Trains an Conventional Neural Network on previously predicted values by the two models
-
-batch_size = 32
-num_classes = 7
-epochs = 3
-
-modele = Sequential()
-modele.add(Dense(128, activation='relu', input_shape=(num_classes*len(model),)))
-modele.add(Dense(num_classes, activation='softmax'))
-
-modele.compile(loss='categorical_crossentropy',
-              optimizer=keras.optimizers.Adadelta(),
-              metrics=['accuracy'])
-
-history = modele.fit(p_train, y_train,
-                    batch_size=batch_size,
-                    epochs=epochs,
-                    verbose=1,
-                    validation_data=(p_test, y_test))
-
-score = modele.evaluate(p_test, y_test, verbose=0)
-m.save('ensemble.h5')
-
-print('NN Based Ensembled Model')
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+# # Some preprocessing
+# input_shape = (img_rows, img_cols, 1)
+#
+# x_train = x_train.astype('float32')
+# x_test = x_test.astype('float32')
+# x_train_rev = x_train_rev.astype('float32')
+# x_test_rev = x_test_rev.astype('float32')
+# x_train /= 255
+# x_test /= 255
+# x_train_rev /= 255
+# x_test_rev /= 255
+# print('x_train shape:', x_train.shape)
+#
+# y_train = keras.utils.to_categorical(y_train, num_classes)
+# y_test = keras.utils.to_categorical(y_test, num_classes)
+# print('y_train shape:', y_train.shape)
+#
+# # define the model
+# def cnn_model():
+#     model = Sequential()
+#
+#     model.add(BatchNormalization(input_shape=input_shape))
+#
+#     model.add(Conv2D(32, kernel_size=(3, 3)))
+#     model.add(BatchNormalization())
+#     model.add(Activation('relu'))
+#
+#     model.add(Conv2D(64, (3, 3)))
+#     model.add(BatchNormalization())
+#     model.add(Activation('relu'))
+#     model.add(MaxPooling2D(pool_size=(2, 2)))
+#     model.add(Dropout(0.25))
+#
+#     model.add(Conv2D(512, (3, 3)))
+#     model.add(BatchNormalization())
+#     model.add(Activation('relu'))
+#     model.add(MaxPooling2D(pool_size=(2, 2)))
+#     model.add(Dropout(0.25))
+#
+#     model.add(Conv2D(64, (3, 3)))
+#     model.add(BatchNormalization())
+#     model.add(Activation('relu'))
+#     model.add(Dropout(0.25))
+#     # model.add(Conv2D(32, (3, 3)))
+#     # model.add(BatchNormalization())
+#     # model.add(Activation('relu'))
+#     # model.add(Dropout(0.2))
+#
+#     model.add(Flatten())
+#     model.add(Dense(512))
+#     model.add(BatchNormalization())
+#     model.add(Activation('relu'))
+#     model.add(Dropout(0.25))
+#
+#     model.add(Dense(128))
+#     model.add(BatchNormalization())
+#     model.add(Activation('relu'))
+#     model.add(Dropout(0.25))
+#
+#     model.add(Dense(num_classes, activation='softmax'))
+#
+#     model.compile(loss=keras.losses.categorical_crossentropy,
+#                   optimizer=keras.optimizers.adam(),
+#                   metrics=['accuracy'])
+#
+#     return model
+#
+# # function to plot graph
+# def plotGraph(history):
+#     plt.plot(history.history['acc'])
+#     plt.plot(history.history['val_acc'])
+#     plt.title('model accuracy')
+#     plt.ylabel('accuracy')
+#     plt.xlabel('epoch')
+#     plt.legend(['train', 'test'])
+#     plt.show()
+#
+#     # summarize history for loss
+#     plt.plot(history.history['loss'])
+#     plt.plot(history.history['val_loss'])
+#     plt.title('model loss')
+#     plt.ylabel('loss')
+#     plt.xlabel('epoch')
+#     plt.legend(['train', 'test'])
+#     plt.show()
+#
+# # We will train two models one on normal images another on reversed images and finally a NN on predicted values from these models
+#
+# batch_size = 64
+# epochs = 25
+# model = []
+#
+# print("=======| Model 1 |=========")
+# modelc = cnn_model()
+# history = modelc.fit(x_train, y_train,
+#           batch_size=batch_size,
+#           epochs=epochs,
+#           verbose=1,
+#           validation_split = 0.1)
+# model.append(modelc)
+# plotGraph(history)
+#
+# print("=======| Model 2 |=========")
+# modelc = cnn_model()
+# history = modelc.fit(x_train_rev, y_train,
+#           batch_size=batch_size,
+#           epochs=epochs,
+#           verbose=1,
+#           validation_split = 0.1)
+# model.append(modelc)
+# plotGraph(history)
+#
+# # p_tr >> prediction on training data
+# # p_te >> prediction on test data
+#
+# p_tr = []
+# p_te = []
+#
+# for i, m in enumerate(model):
+#     if i ==0:
+#         p = m.predict(x_train)
+#         pt = m.predict(x_test)
+#     else:
+#         p = m.predict(x_train_rev)
+#         pt = m.predict(x_test_rev)
+#     p_tr.append(p)
+#     p_te.append(pt)
+# #     m.save('cnn'+str(i)+'.h5')
+#
+# print(len(model))
+#
+# p_train = np.zeros((y_train.shape[0],num_classes*len(model)))
+# p_test = np.zeros((y_test.shape[0],num_classes*len(model)))
+# for i, p in enumerate(p_tr):
+#     print(i)
+#     p_train[:,num_classes*i:num_classes*(i+1)] = p
+#
+# for i, p in enumerate(p_te):
+#     p_test[:,num_classes*i:num_classes*(i+1)] = p
+#
+# print(p_train.shape, p_test.shape)
+#
+# # Trains an Conventional Neural Network on previously predicted values by the two models
+#
+# batch_size = 32
+# num_classes = 7
+# epochs = 3
+#
+# modele = Sequential()
+# modele.add(Dense(128, activation='relu', input_shape=(num_classes*len(model),)))
+# modele.add(Dense(num_classes, activation='softmax'))
+#
+# modele.compile(loss='categorical_crossentropy',
+#               optimizer=keras.optimizers.Adadelta(),
+#               metrics=['accuracy'])
+#
+# history = modele.fit(p_train, y_train,
+#                     batch_size=batch_size,
+#                     epochs=epochs,
+#                     verbose=1,
+#                     validation_data=(p_test, y_test))
+#
+# score = modele.evaluate(p_test, y_test, verbose=0)
+# m.save('ensemble.h5')
+#
+# print('NN Based Ensembled Model')
+# print('Test loss:', score[0])
+# print('Test accuracy:', score[1])
